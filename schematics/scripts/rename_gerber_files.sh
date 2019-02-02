c51c5a1f3ae08f28a24e5b874683f58e04dd7434
Istvan Szenasi <szeist@gmail.com>
2019-02-02 19:22:40 +0100

    add display pcb design with gerber files

---
+++ schematics/scripts/rename_gerber_files.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+GERBER_DIR=gerber
+
+cd $GERBER_DIR
+
+mv CHPLX12-F.Cu.gtl CHPLX12_Top_Copper.cmp
+mv CHPLX12-B.Cu.gbl CHPLX12_Bottom_Copper.sol
+mv CHPLX12-F.Mask.gts CHPLX12_Top_Soldermask.stc
+mv CHPLX12-B.Mask.gbs CHPLX12_Bottom_Soldermask.sts
+mv CHPLX12-B.SilkS.gbo CHPLX12_Top_Silkscreen.plc 
+mv CHPLX12-Edge.Cuts.gm1 CHPLX12_Outline_Milling.milling
+mv CHPLX12_Top_Silkscreen.plc CHPLX12_Bottom_Silkscreen.plc
+mv CHPLX12-F.SilkS.gto CHPLX12_Bottom_Silkscreen.pls
+
+cd -
